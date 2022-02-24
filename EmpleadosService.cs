61192d9ae2bd32d3a1eaf9d8b1dfbd4e4bc60164
Hugoxoft <56564377+hugoxoft@users.noreply.github.com>
2022-02-24 16:12:06 -0600

    Data/Services
    Crear la carpeta Data/Services

---
+++ EmpleadosService.cs
@@ -0,0 +1,47 @@
+﻿using BlazorCrud5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BlazorCrud5.Data.Services
+{
+    public class EmpleadosService : IEmpleadosService
+    {
+        //Configuramos el contexto de datos para conectarnos a la base de datos
+        //que esta declarado en Startup.cs.
+        private readonly AppDbContext _context;
+        public EmpleadosService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<Empleado> Actualizar(int id, Empleado empleadoactualizar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empleado> Crear(Empleado empleadocrear)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empleado> Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empleado> ObtenerId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Empleado>> ObtenerTodos()
+        {
+            return await _context.Empleados.ToListAsync();
+        }
+
+    }
+}
---
+++ IEmpleadosService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorCrud5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud5.Data.Services
+{
+    public interface IEmpleadosService
+    {
+        //Devuelve una lista de empleados
+        public Task<List<Empleado>> ObtenerTodos();
+        //Recibe el id del empleado para obtener un empleado
+        public Task<Empleado> ObtenerId(int id);
+        //Recibe el objeto de tipo empleado que se insertara
+        public Task<Empleado> Crear(Empleado empleadocrear);
+        //Recibe el id y el objeto de tipo empleado que trae los datos a actualizar
+        public Task<Empleado> Actualizar(int id, Empleado empleadoactualizar);
+        //Recibe el id del empleado a eliminar
+        public Task<Empleado> Eliminar(int id);
+    }
+}
